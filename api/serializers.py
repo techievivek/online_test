ef84147376da568007668aa0daeb0021d8cf67bf
techievivek <techievivek123@gmail.com>
2020-05-01 13:03:58 +0530

    added serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,34 @@
 from rest_framework import serializers
-from yaksh.models import Course
+from yaksh.models import Course,LearningModule,LearningUnit,Lesson,Quiz
+class QuizSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Quiz
+        fields=['description','pass_criteria','attempts_allowed']
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Lesson
+        fields=['name','description','html_data','video_file']
+class LearningUnitSerializer(serializers.ModelSerializer):
+    lesson=LessonSerializer()
+    quiz=QuizSerializer()
+    class Meta:
+        model=LearningUnit
+        fields=['lesson','quiz']
+class LearningModuleSerializer(serializers.ModelSerializer):
+    learning_unit=LearningUnitSerializer(many=True)
+    class Meta:
+        model=LearningModule
+        fields=['name','description','learning_unit']
 class CourseSerializer(serializers.ModelSerializer):
+    creator=serializers.StringRelatedField()
+    # students=serializers.StringRelatedField(many=True)
+    # requests=serializers.StringRelatedField(many=True)
+    # rejected=serializers.StringRelatedField(many=True)
+    # teachers=serializers.StringRelatedField(many=True)
+    # learning_module=serializers.StringRelatedField(many=True)
+    learning_module=LearningModuleSerializer(many=True)
+    # grading_system=serializers.StringRelatedField()
     class Meta:
         model=Course
-        fields='__all__'
+        fields=['name','enrollment','active','code','hidden','creator','created_on','is_trial','instructions','view_grade','learning_module',
+        'start_enroll_time','end_enroll_time']
--- api/views.py
+++ api/views.py
@@ -1,24 +1,19 @@
 # Create your views here.
 from yaksh.models import Course
 from .serializers import CourseSerializer
-from django.http import Http404
-from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-
-
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 class CourseList(APIView):
-    """
-    List all snippets, or create a new snippet.
-    """
-    def get(self, request, format=None):
-        courses = Course.objects.all()
-        serializer = CourseSerializer(courses, many=True)
+    permission_classes=[AllowAny]
+    def get(self,request):
+        data=Course.objects.all()
+        serializer=CourseSerializer(data,many=True)
         return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = CourseSerializer(data=request.data)
-        if serializer.is_valid():
+    def post(self,request):
+        serializer=CourseSerializer(data=request.data)
+        if(serializer.is_valid()):
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer._validated_data)
+        return Response(serializer.errors)    
+
