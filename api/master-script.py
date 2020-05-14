28649620064d845da7d19f46b9da490444333096
techievivek <techievivek123@gmail.com>
2020-05-14 16:04:44 +0530

    remove unnecessary code from master script

--- api/master-script.py
+++ api/master-script.py
@@ -19,43 +19,12 @@ def create_connection(db_file):
     return conn
 
 def select_all_data(conn):
-    data=[] # to store the each row entry
-    conn.row_factory = sqlite3.Row #https://docs.python.org/2/library/sqlite3.html#sqlite3.Row
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM workshop_app_proposeworkshopdate")
-    rows = cur.fetchall()
-    for row in rows:
-        dict={}
-        for member in row.keys():
-            cur2=conn.cursor() #second cursor to read from another model.
-            #if it is a relation to other model.
-            if member=='proposed_workshop_coordinator_id':
-                cur2.execute('select * from auth_user where id=?',(row[member],))
-                x=cur2.fetchone()
-                dict['proposed_workshop_coordinator_username'] =x['username']
-            elif member=='proposed_workshop_instructor_id':
-                cur2.execute('select * from auth_user where id=?',(row[member],))
-                x=cur2.fetchone()
-                dict['proposed_workshop_instructor_username'] =x['username']
-            elif member=='proposed_workshop_title_id':
-                cur2.execute('select * from workshop_app_workshoptype where id=?',(row[member],))
-                x=cur2.fetchone()
-                #extra fields I have added for more info on workshop.
-                dict["proposed_workshop_name"] =x['workshoptype_name']
-                dict["proposed_workshoptype_description"]=x["workshoptype_description"]
-                dict["proposed_workshop_duration"] =x['workshoptype_duration']
-            else:
-                dict[member]=row[member]
-        data.append(dict)
-    ''' for row in data:
-     for key,value in row.items():
-      print(" "+key.capitalize()+": "+str(value))
-     print("")'''
-    # print(json.dumps(data)) #for json data, will add more from here.
     url=r'http://localhost:8000/api/course/create/'
     try:
+        # read the demo json file
         with open('demo-data.json') as f:
             json_data=json.load(f)
+        #create a post request to the api end-point.
         response=requests.post("http://localhost:8000/api/course/create/",json=json_data)
         # If the response was successful, no Exception will be raised
         response.raise_for_status()
